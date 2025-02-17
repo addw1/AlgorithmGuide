718cc6904387a40580a887d74bcc9b3d4d70cab1
Qingning <614639560@qq.com>
2025-02-17 17:43:39 -0500

    Updated 1152

--- slides/hw2/hw2/problem4/run_matrix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if the executable file exists
-if [ -f "./matrix" ]; then
-    echo "Running ./matrix ..."
-    ./matrix
-else
-    echo "Error: ./matrix file does not exist or is not executable."
-fi
